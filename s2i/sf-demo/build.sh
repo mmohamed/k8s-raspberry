4cb97a3f88e40c14c617a87ac1b8265ca99baaa1
Marouan MOHAMED <medmarouen@gmail.com>
2020-03-16 19:01:00 +0100

    Up Home

--- s2i/sf-demo/build.sh
+++ s2i/sf-demo/build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-dir=$(pwd)
-
 # Clear app folder
-rm -rf $dir/public/*
+rm -rf ./public
 
 # Clone app
-git clone -b k8s --single-branch https://github.com/mmohamed/demo.git $dir/public
+git clone -b k8s --single-branch https://github.com/mmohamed/demo.git public
 
 docker build . -t medinvention/sfdemo:arm
 docker push medinvention/sfdemo:arm
